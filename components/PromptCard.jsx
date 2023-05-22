e230e62ca67f6c890f0e4b87dac7bcefc05af495
desperado1802 <mumun_ali@hotmail.com>
2023-05-22 16:30:11 +0300

    removed the required for the username in userSchema

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -21,13 +22,19 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image
-            src={post.creator.image}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
+          <Link
+            href={
+              session?.user?.id === post.creator._id ? "/profile" : "/profiles"
+            }
+          >
+            <Image
+              src={post.creator.image}
+              alt="user_image"
+              width={40}
+              height={40}
+              className="rounded-full object-contain"
+            />
+          </Link>
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900 ">
               {post.creator.username}
@@ -54,7 +61,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
       {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
